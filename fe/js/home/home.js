2cb730b9db0823e814e41728607788a9111f9866
wmx <wmx@zydeMacBook-Pro.local>
2017-02-08 10:48:46 +0800

    调百度定位接口获取当前城市码

--- fe/js/home/home.js
+++ fe/js/home/home.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import $ from 'jquery';
 import style from '../../css/home.less';
 
 export default class Home extends React.Component{
+	componentWillMount() {
+		$.getJSON('http://api.map.baidu.com/location/ip?ak=ymQ8eekrnKms0Vb5GAIaQMCr3Yipl3IN&coor=bd09ll&callback=?',
+			function(data) {
+				//console.log(data);
+				if(data.status === 0) {
+					let location = data.content,
+						cityCode = location.address_detail.city_code;
+					alert('城市码：' + cityCode);
+				}
+			});
+	}
 	render() {
 		let sss;
 		sss = 1;
